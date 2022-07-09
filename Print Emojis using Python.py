da8a264ccffa1ae0cd5b1e201ec2948a81020f8e
Cansın Nuh Tufan <42410283+thecansin@users.noreply.github.com>
2022-07-09 21:48:58 +0300

    Add files via upload

---
+++ Print Emojis using Python.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 21:43:17 2022
+
+"""
+import emoji
+
+print(emoji.emojize("I love reading books :books:"))
+print(emoji.emojize("Some people have a very sensitive heart :red_heart:, please be kind with them. :hibiscus:"))
